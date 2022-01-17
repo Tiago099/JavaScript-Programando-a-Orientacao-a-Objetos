9dec4192ee7113db3000af779e44b062df34e77b
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-17 19:22:02 -0300

    Repetição de código

---
+++ index.js
@@ -0,0 +1,31 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Tiago";
+cliente1.cpf = 12345645610;
+cliente1.agencia = 1202;
+cliente1.saldo = 0;
+
+
+cliente2.nome = "Alice";
+cliente2.cpf = 78945612350;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+cliente2.nome = "Joana";
+cliente2.cpf = 78945648950;
+cliente2.agencia = 1801;
+cliente2.saldo = 10;
+
+
+
+console.log(cliente2);
+
