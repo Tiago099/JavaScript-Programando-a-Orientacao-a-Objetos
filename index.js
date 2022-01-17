34bef3121bd55b01d887124e5d366b136490b167
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-17 20:44:45 -0300

    Adicionando comportamentos

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
+    
 
 }
+class  ContaCorrente{
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+            this._saldo += valor;
+           
+        }
+    }
+
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Tiago";
 cliente1.cpf = 12345645610;
 cliente1.agencia = 1202;
 cliente1.saldo = 0;
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 78945612350;
 cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
-cliente2.nome = "Joana";
-cliente2.cpf = 78945648950;
-cliente2.agencia = 1801;
-cliente2.saldo = 10;
+const ContaCorrenteTiago = new ContaCorrente();
+ContaCorrenteTiago.agencia=1202
+
+
+ContaCorrenteTiago.depositar(-110);
+ContaCorrenteTiago.depositar(110);
+ContaCorrenteTiago.depositar(110);
+
+
+const valorSacado = ContaCorrenteTiago.sacar(50);
+console.log(valorSacado);
 
 
 
-console.log(cliente2);
+console.log(ContaCorrenteTiago);
+
 
