652c5fa95d303e962ca614a8ee30641905bf3807
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-17 23:38:44 -0300

    Modularizando código

--- index.js
+++ index.js
@@ -1,56 +1,32 @@
-class Cliente{
-    nome;
-    cpf;
-    
-
-}
-class  ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if (this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-            this._saldo += valor;
-           
-        }
-    }
-
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Tiago";
 cliente1.cpf = 12345645610;
-cliente1.agencia = 1202;
-cliente1.saldo = 0;
+
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 78945612350;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 const ContaCorrenteTiago = new ContaCorrente();
-ContaCorrenteTiago.agencia=1202
+ContaCorrenteTiago.agencia=1202;
+ContaCorrenteTiago.cliente = cliente1;
 
 
-ContaCorrenteTiago.depositar(-110);
-ContaCorrenteTiago.depositar(110);
-ContaCorrenteTiago.depositar(110);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 1001;
+conta2.depositar(500);
 
-
-const valorSacado = ContaCorrenteTiago.sacar(50);
-console.log(valorSacado);
-
+conta2.tranferir(300, ContaCorrenteTiago);
 
 
 console.log(ContaCorrenteTiago);
+
+
+
+
 
 
