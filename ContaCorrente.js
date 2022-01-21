c86c83397866702cce36c8bc7b43edc5a6fcb54c
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-21 20:24:57 -0300

    Construtores e estáticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,8 +1,11 @@
 import { Cliente } from "./Cliente.js"; 
  
 export class  ContaCorrente{
+   static numeroDeContas = 0;
     agencia;
     _cliente;
+
+    _saldo = 0;
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -22,6 +25,11 @@ export class  ContaCorrente{
         return this._saldo;
     }
 
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
     
     sacar(valor){
         if (this._saldo >= valor){
--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Tiago";
-cliente1.cpf = 12345645610;
-
-
-//const cliente2 = new Cliente();
-//cliente2.nome = "Alice";
-//cliente2.cpf = 78945612350;
-
-const ContaCorrenteTiago = new ContaCorrente();
-ContaCorrenteTiago.agencia=1202;
-ContaCorrenteTiago.cliente = cliente1;
-
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 1001;
+const cliente1 = new Cliente("Tiago", 12345645610);
+const cliente2 = new Cliente("Alice", 78945612350);
+const ContaCorrenteTiago = new ContaCorrente(1202, cliente1);
+const conta2 = new ContaCorrente(1001, cliente2);
 conta2.depositar(500);
 
 
 conta2.tranferir(300, ContaCorrenteTiago);
 
 
-console.log(conta2);
 
 
+console.log(ContaCorrente.numeroDeContas);
 
 
 
