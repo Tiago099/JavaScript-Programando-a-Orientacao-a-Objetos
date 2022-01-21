761c10967022471c366c850eeb4d68e8c3cce296
Tiago Sebastian <tiagosebastian99@gmail.com>
2022-01-21 17:53:52 -0300

    Acessando Atributos privados

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ cliente1.nome = "Tiago";
 cliente1.cpf = 12345645610;
 
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 78945612350;
+//const cliente2 = new Cliente();
+//cliente2.nome = "Alice";
+//cliente2.cpf = 78945612350;
 
 const ContaCorrenteTiago = new ContaCorrente();
 ContaCorrenteTiago.agencia=1202;
@@ -16,14 +16,15 @@ ContaCorrenteTiago.cliente = cliente1;
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 1001;
 conta2.depositar(500);
+
 
 conta2.tranferir(300, ContaCorrenteTiago);
 
 
-console.log(ContaCorrenteTiago);
+console.log(conta2);
 
 
 
